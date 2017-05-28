7ac964d05faee417cd74e9da0d16897f1070e726
Jack Wong <jackwong3101@yahoo.com>
2017-05-28 12:37:14 +0800

    added mailer with letter_opener

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,14 +4,14 @@ class FlightsController < ApplicationController
   	@airports = Airport.all 
   	@airport_options = @airports.map{|a|[a.code,a.id]}.uniq{|a| a.first}
 
-  	unless (params.empty?)
-  		if params.has_key?(:flight)
-  			if params[:flight].has_key?(:search)
-	   			typed_departing_airport_id = Airport.find_by(code: params[:flight][:search].upcase).id if !params[:flight][:search].empty?
-	   		end
-	   	end
-  		@queried_flights =  Flight.where("departing_airport_id IN (?) AND departure_date = ?",[params[:flight][:departing_airport_id],typed_departing_airport_id],submitted_date)
-  	end
+  	
+		if params.has_key?(:flight)
+			if params[:flight].has_key?(:search)
+   			typed_departing_airport_id = Airport.find_by(code: params[:flight][:search].upcase).id if !params[:flight][:search].empty?
+   		end
+		@queried_flights =  Flight.where("departing_airport_id IN (?) AND departure_date = ?",[params[:flight][:departing_airport_id],typed_departing_airport_id],submitted_date)
+    end
+  	
   end
 
   def create
